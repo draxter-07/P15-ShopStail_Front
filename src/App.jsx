ed8e91bea8bd36593203f92f211f45ac17711ef7
Philippe <philippe.idalgoprestes@gmail.com>
2023-07-14 17:23:32 -0300

    Início checkout e carrinho

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import styled from "styled-components"
-import HomePage from "./pages/HomePage"
-import SignInPage from "./pages/SignInPage"
-import SignUpPage from "./pages/SignUpPage"
-import TransactionsPage from "./pages/TransactionPage"
+import CheckoutPage from "./pages/CheckoutPage"
+import CarrinhoPage from "./pages/CarrinhoPage"
 
 export default function App() {
   return (
