fbc36a115f18a94dc58c2da388ef4139e43e601c
Philippe <philippe.idalgoprestes@gmail.com>
2023-07-14 17:24:34 -0300

    Início carrinho e checkout

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,12 @@ import CarrinhoPage from "./pages/CarrinhoPage"
 
 export default function App() {
   return (
-    <PagesContainer>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/carrinho" element={<CarrinhoPage />} />
-          <Route path="/checkout" element={<CheckoutPage />} />
-        </Routes>
-      </BrowserRouter>
-    </PagesContainer>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/carrinho" element={<CarrinhoPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-const PagesContainer = styled.main`
-  background-color: #8c11be;
-  width: calc(100vw - 50px);
-  max-height: 100vh;
-  padding: 25px;
-`
