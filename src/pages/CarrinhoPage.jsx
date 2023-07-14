1f2844a3ed5403086d2e3e60481098810511afc7
Philippe <philippe.idalgoprestes@gmail.com>
2023-07-14 18:42:59 -0300

    Finalização parcial da página Carrinho

--- src/pages/CarrinhoPage.jsx
+++ src/pages/CarrinhoPage.jsx
@@ -3,8 +3,8 @@ import Topo from "./components/Topo.jsx"
 
 export default function CarrinhoPage() {
   const itens = [
-    {title: "titulo", image: "https://4429028l.ha.azioncdn.net/img/2022/11/produto/11627/camiseta-preta.jpg?ims=500x700", quantity: 5},
-    {title: "titulo", image: "https://4429028l.ha.azioncdn.net/img/2022/11/produto/11627/camiseta-preta.jpg?ims=500x700", quantity: 5}
+    {title: "título", image: "https://4429028l.ha.azioncdn.net/img/2022/11/produto/11627/camiseta-preta.jpg?ims=500x700", quantity: 5},
+    {title: "título", image: "https://4429028l.ha.azioncdn.net/img/2022/11/produto/11627/camiseta-preta.jpg?ims=500x700", quantity: 5}
   ];
   const PageContent = styled.div`
     box-sizing: border-box;
@@ -45,6 +45,44 @@ export default function CarrinhoPage() {
         display: flex;
         flex-direction: column;
         align-items: flex-start;
+        h1{
+          font-size: 25px;
+          margin: 0px 0px 10px 0px;
+        }
+        h2{
+          font-size: 15px;
+        }
+      }
+      button{
+        padding: 5px 10px;
+        background-color: rgb(255, 85, 50);
+        border: none;
+        border-radius: 5px;
+        color: rgb(255, 255, 255);
+        font-size: 15px;
+        :hover{
+          background-color: rgb(70, 70, 255);
+        }
+      }
+    }
+  `
+  const Botoes = styled.div`
+    box-sizing: border-box;
+    width: 50%;
+    height: auto;
+    margin: 20px 0px 0px 0px;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    button{
+      padding: 5px 10px;
+      background-color: rgb(255, 85, 50);
+      border: none;
+      border-radius: 5px;
+      color: rgb(255, 255, 255);
+      font-size: 15px;
+      :hover{
+        background-color: rgb(70, 70, 255);
       }
     }
   `
@@ -59,7 +97,7 @@ export default function CarrinhoPage() {
                 <img src={item.image}></img>
                 <div>
                   <h1>{item.title}</h1>
-                  <h2>{item.quantity}</h2>
+                  <h2>Quantidade: {item.quantity}</h2>
                 </div>
               </div>
               <div>
@@ -68,6 +106,9 @@ export default function CarrinhoPage() {
             </Item>
             )}
           </ItensCompra>
+          <Botoes>
+            <button>Finalizar compra</button>
+          </Botoes>
       </PageContent>
     </>
   )
